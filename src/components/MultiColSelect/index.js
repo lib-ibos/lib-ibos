7e5e3912ae569d4223cb770fec8aa0c4043a8f27
FEN <guangyaol@olymtech.com>
2016-11-21 13:15:11 +0800

    MultiColSelect 各接口调通，上下键的逻辑还有问题

--- src/components/MultiColSelect/index.js
+++ src/components/MultiColSelect/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {render}  from 'react-dom';
 
 import { Dropdown,Input} from 'antd';
+// const MenuItem = Menu.Item;
 import Menu ,{MenuItem} from 'rc-menu';
 import KeyCode from 'rc-util/lib/KeyCode';
 import './styles/index'
@@ -14,34 +15,15 @@ class RichSelect extends Component {
         this.state = {
             val:"",
             optionList: {},
-            selectedKeys:['0'],
-            open:false
+            open:false,
+            selectKeys:['-1'],
+            activeKeys:'-1',
         }
     }
 
     static defaultProps ={
         selectKey:"value",
         disabled:false
-    }
-
-    handleFocus = (e) =>{
-        e.target.select();
-
-        // 为了首次触发去请求数据
-        this.handleChange(e)
-    }
-
-    handleChange =(e)=>{
-        this.setState({
-            val:e.target.value,
-            selectedKeys:['0']
-        })
-       if(this.props.onChange ) {
-           let data = this.props.onChange();
-           this.setState({
-               optionList:data
-           })
-       }
     }
 
     setOpenState=(visible)=>{
@@ -84,26 +66,26 @@ class RichSelect extends Component {
                 event.stopPropagation();
                 return;
             }else{
-                var currentKey = +this.state.selectedKeys[0];
-                var _length = this.state.optionList.body.length - 1;
+                var _length = this.props.dataBody.length - 1;
+                var currentKey = +this.state.activeKey || _length;
 
-                // console.log(currentKey ,_length,currentKey == _length)
+                console.log(+this.state.activeKey ,_length,currentKey == _length)
                 if(keyCode === KeyCode.DOWN){
                     if(currentKey == _length ){
                         currentKey = 0;
                     }else {
-                        currentKey = +this.state.selectedKeys[0] +1
+                        currentKey += 1;
                     }
                 }else{
                     if(currentKey == 0){
                         currentKey = _length ;
                     }else {
-                        currentKey = +this.state.selectedKeys[0] -1
+                        currentKey -= 1
                     }
                 }
 
                 this.setState({
-                    selectedKeys:[String(currentKey)]
+                    activeKey:String(currentKey)
                 })
             }
         } else if (keyCode === KeyCode.ESC) {
@@ -114,14 +96,17 @@ class RichSelect extends Component {
             }
             return;
         }else if (keyCode === KeyCode.ENTER){
-            let _obj = this.state.optionList.body;
-            let currentKey = this.state.selectedKeys;
-            this.setOpenState(false);
-            console.log(_obj[currentKey][this.props.selectKey])
+            let _key = this.props.selectKey;
+            let _obj = this.props.dataBody;
+            let currentKey = this.state.activeKey;
             this.setState({
-                val:_obj[currentKey][this.props.selectKey]
+                open:false,
+                selectKeys:[String(currentKey)]
             })
-            this.props.onCustomSelect && this.props.onCustomSelect(_obj[currentKey]);
+            this.props.onChange(_obj[currentKey][_key])
+            event.preventDefault();
+            event.stopPropagation();
+            // this.props.onCustomSelect && this.props.onCustomSelect(_obj[currentKey]);
         }
 
     }
@@ -133,13 +118,30 @@ class RichSelect extends Component {
         }
     }
 
+    handleChange=(e)=>{
+        this.props.onChange(e.target.value)
+    }
+
+    handleSelect =(value) =>{
+        let _key = this.props.selectKey;
+        let _obj = this.props.dataBody[value.key]
+        let currentKey = _obj[_key]
+        this.setState({
+            open:false,
+            activeKey:String(value.key),
+            selectKeys:[String(value.key)]
+        })
+        this.props.onChange(currentKey)
+    //that.props.onCustomSelect && that.props.onCustomSelect(_obj);
+}
+
     render() {
-        const that = this;
-        const dropdownHeadData = this.state.optionList.head,
-            dropdownBodyData = this.state.optionList.body;
+        const {dataHeader,dataBody,type,rows,autosize,...props} = this.props;
+        const dropdownHeadData = dataHeader,
+            dropdownBodyData = dataBody || "not found";
         // 拼接下拉框header部分结构
         const dropdownHeadElement = dropdownHeadData && dropdownHeadData.map(val => <p key={val.dataIndex}>{val.title}</p>);
-        const dropdownBodyElement = dropdownBodyData && bodyElement();
+        const dropdownBodyElement = typeof dropdownBodyData ==="string" ? <MenuItem className="no-data" disabled><p colSpan="2">{dropdownBodyData}</p><p></p></MenuItem> : bodyElement();
         //下拉框body部分
         function bodyElement() {
             return dropdownBodyData.map(function (val,index) {
@@ -149,25 +151,14 @@ class RichSelect extends Component {
             })
         }
 
-        function handleSelect(value){
-            let _key = that.props.selectKey;
-            let _obj = dropdownBodyData[value.key]
-            let currentKey = dropdownBodyData[value.key][_key]
-            that.setState({
-                val:currentKey,
-                open:false,
-                selectedKeys:[String(value.key)]
-            })
-            that.props.onCustomSelect && that.props.onCustomSelect(_obj);
-        }
 
         return (
             <Dropdown overlay={
                 <Menu
-                    multiple
-                    onSelect={handleSelect}
+                    onSelect={this.handleSelect}
                     className="o-dropdown-multi-col"
-                    selectedKeys={this.state.selectedKeys}
+                    activeKey ={this.state.activeKey}
+                    selectedKeys={this.state.selectKeys}
                 >
                     <MenuItem key="title" disabled>{dropdownHeadElement}</MenuItem>
                     {dropdownBodyElement}
@@ -179,11 +170,13 @@ class RichSelect extends Component {
             >
 
                 <Input
-                       {...this.props}
-                       onChange = {this.handleChange}
-                       value = {this.state.val}
-                       onFocus = {this.handleFocus}
-                       onKeyDown={this.onKeyDown}
+                    ref="input"
+                    type={type}
+                    value={props.value}
+                    rows={rows}
+                    autosize={autosize}
+                    onChange={this.handleChange}
+                    onKeyDown={this.onKeyDown}
                 />
             </Dropdown>
         );

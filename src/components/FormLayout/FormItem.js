93f2af8f11b4e2b0a616b0bfef303291e7994dd1
fen <guangyaol@olymtech.com>
2016-11-25 16:13:54 +0800

    新增label=" "空格的时候，去掉labelwidth 级后面的冒号

--- src/components/FormLayout/FormItem.js
+++ src/components/FormLayout/FormItem.js
@@ -3,30 +3,29 @@ import classnames from 'classnames';
 import {Form} from 'antd';
 const AntFormItem = Form.Item;
 
-import {checkSecurity} from'../share'
 
 class FormItem extends Component {
     static defaultProps={
         labelVertical:false
     }
     render() {
-        const {security, children, labelVertical,labelWidth,...props} = this.props
-        const {canAccess} = checkSecurity(this.props)
-        if (!canAccess) {
-            return <noscript/>
-        }
-        const _style = props.label ? {} : {paddingLeft: 0}
+        const {labelVertical,labelWidth,label,...props} = this.props
+        const _style = label ? {} : {paddingLeft: 0}
         const _class = classnames(
             {"o-form-label--vertical":labelVertical},
-            {["o-form-label--"+labelWidth]:labelWidth}
+            {["o-form-label--"+labelWidth]:labelWidth},
+            {"o-form-label--none":(label == " ")}
         )
+        const _label = label && label
+
         return (
             <AntFormItem
                 {...props}
                 style={_style}
+                label={_label}
                 className={_class}
             >
-                { children}
+                { props.children}
             </AntFormItem>
         )
     }

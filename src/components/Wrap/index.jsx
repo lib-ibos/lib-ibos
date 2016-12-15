491fbbbcda2391b07a1456fcc06d13cb0f5fd630
FEN <guangyaol@olymtech.com>
2016-12-15 11:10:39 +0800

    原来的Panel组件更名为Wrap

--- src/components/Wrap/index.jsx
+++ src/components/Wrap/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
-export default function Panel({children, ...props}) {
+export default function Wrap({children, ...props}) {
     return <div {...props}>{children}</div>
 }

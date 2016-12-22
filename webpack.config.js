dc0779b9ace9d61fd9ee51d38e72471f388943e4
codering <zcodeworld@gmail.com>
2016-12-22 10:13:27 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const HtmlwebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: [__dirname + "/src/index.js"], //已多次提及的唯一入口文件
     output: {
-        path: __dirname + "/public",//打包后的文件存放的地方
+        path: __dirname + "/dist",//打包后的文件存放的地方
         pathinfo: true,
         filename: "bundle.js"//打包后输出文件的文件名
     },

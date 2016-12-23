74d7805eb585d7bf48f55aa1de0019e4dd277d72
codering <zcodeworld@gmail.com>
2016-12-23 14:28:04 +0800

    remove old formItem

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,6 @@ import Panel from './Panel'
 import Button from './button'
 import Checkbox from './checkbox'
 import Table from './table'
-import FormItem from './form-item'
 import MultiColSelect from './MultiColSelect'
 import FormLayout from './FormLayout'
 import Wrap from './Wrap'
@@ -15,7 +14,6 @@ export default {
     Checkbox,
     Table,
     FormLayout,
-    FormItem,
     MultiColSelect,
     Wrap,
     Tabs,

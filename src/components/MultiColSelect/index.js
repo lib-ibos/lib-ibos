71fb4fed54d473ef3cf05b5ea300a74652462225
fen <guangyaol@olymtech.com>
2017-01-23 09:30:41 +0800

    update

--- src/components/MultiColSelect/index.js
+++ src/components/MultiColSelect/index.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
-import ReactDOM,{render}  from 'react-dom';
+import ReactDOM, {render}  from 'react-dom';
 
-import { Dropdown,Input} from 'antd';
+import {Dropdown, Input} from 'antd';
 // const MenuItem = Menu.Item;
-import Menu ,{MenuItem} from 'rc-menu';
+import Menu, {MenuItem} from 'rc-menu';
 import KeyCode from 'rc-util/lib/KeyCode';
 import './styles/index'
 
@@ -13,51 +13,51 @@ class MultiColSelect extends Component {
         super(props)
 
         this.state = {
-            val:"",
+            val: "",
             optionList: {},
-            open:false,
-            selectKeys:['-1'],
-            activeKey:'0',
-            canMenuHide:true
+            open: false,
+            selectKeys: ['-1'],
+            activeKey: '0',
+            canMenuHide: true
         }
     }
 
     static propTypes = {
         dataBody: React.PropTypes.array,
-        dataHead:React.PropTypes.object,
-        dropdwonMaxRows:React.PropTypes.number
+        dataHead: React.PropTypes.object,
+        dropdwonMaxRows: React.PropTypes.number
 
     }
 
-    static defaultProps ={
-        selectKey:"value",
-        disabled:false,
-        scrollHeight:32
+    static defaultProps = {
+        selectKey: "value",
+        disabled: false,
+        scrollHeight: 32
     }
 
-    scrollTo =(index)=>{
+    scrollTo = (index) => {
         const menu = ReactDOM.findDOMNode(this.refs.menu);
-        let maxHeight = (this.props.dropdwonMaxRows || 8) -1;
-        let scrollHeight = index > maxHeight ? (index - maxHeight)*this.props.scrollHeight : 0
+        let maxHeight = (this.props.dropdwonMaxRows || 8) - 1;
+        let scrollHeight = index > maxHeight ? (index - maxHeight) * this.props.scrollHeight : 0
 
         menu.scrollTop = scrollHeight
     }
 
-    setOpenState=(visible)=>{
+    setOpenState = (visible) => {
         this.setState({
-            open:visible
+            open: visible
         })
     }
 
-    setActiveState = (index)=>{
+    setActiveState = (index) => {
         this.setState({
-            activeKey:String(index)
+            activeKey: String(index)
         })
 
         this.scrollTo(index)
     }
 
-    onKeyDown=(event) =>{
+    onKeyDown = (event) => {
         const props = this.props;
         if (props.disabled) {
             return;
@@ -72,11 +72,11 @@ class MultiColSelect extends Component {
         }
     }
 
-    getInputDOMNode=()=> {
+    getInputDOMNode = () => {
         return this.inputInstance;
     }
 
-    onInputKeyDown=(event)=> {
+    onInputKeyDown = (event) => {
         const props = this.props;
         if (props.disabled) {
             return;
@@ -91,20 +91,20 @@ class MultiColSelect extends Component {
                 event.preventDefault();
                 event.stopPropagation();
                 return;
-            }else{
+            } else {
                 var _length = this.props.dataBody.length - 1;
                 var currentKey = +this.state.activeKey;
 
-                if(keyCode === KeyCode.DOWN){
-                    if(currentKey == _length ){
+                if (keyCode === KeyCode.DOWN) {
+                    if (currentKey == _length) {
                         currentKey = 0;
-                    }else {
+                    } else {
                         currentKey += 1;
                     }
-                }else{
-                    if(currentKey == 0){
-                        currentKey = _length ;
-                    }else {
+                } else {
+                    if (currentKey == 0) {
+                        currentKey = _length;
+                    } else {
                         currentKey -= 1
                     }
                 }
@@ -118,69 +118,83 @@ class MultiColSelect extends Component {
                 event.stopPropagation();
             }
             return;
-        }else if (keyCode === KeyCode.ENTER){
+        } else if (keyCode === KeyCode.ENTER) {
             let _key = this.props.selectKey;
             let _obj = this.props.dataBody;
             let currentKey = this.state.activeKey;
             this.setState({
-                open:false,
-                selectKeys:[String(currentKey)]
+                open: false,
+                selectKeys: [String(currentKey)]
             })
-            this.props.onChange &&　this.props.onChange(_obj[currentKey][_key])
+            this.props.onChange && this.props.onChange(_obj[currentKey][_key])
             this.props.onSelect && this.props.onSelect(_obj[_key]);
             event.preventDefault();
             event.stopPropagation();
         }
     }
 
-    openIfHasChildren =()=> {
+    openIfHasChildren = () => {
         const props = this.props;
         if (React.Children.count(props.children) || isSingleMode(props)) {
             this.setOpenState(true);
         }
     }
 
-    handleChange=(e)=>{
-        this.props.onChange &&　this.props.onChange(e.target.value);
+    handleChange = (e) => {
+        this.props.onChange && this.props.onChange(e.target.value);
         this.props.dataBody.length > 0 && this.setOpenState(true)
         this.setState({
-            selectKeys:['-1'],
+            selectKeys: ['-1'],
         })
     }
 
 
-    onMenuSelect =(value) =>{
+
+    onMenuSelect = (value) => {
         let _key = this.props.selectKey;
         let _obj = this.props.dataBody[value.key]
         let currentKey = _obj[_key]
         this.setState({
-            open:false,
-            selectKeys:[String(value.key)]
+            open: false,
+            selectKeys: [String(value.key)]
         })
         this.setActiveState(value.key)
-        this.props.onChange &&　this.props.onChange(currentKey)
+        this.props.onChange && this.props.onChange(currentKey)
         this.props.onSelect && this.props.onSelect(_obj);
-}
+    }
 
-    handleBlur = ()=>{
-        this.state.canMenuHide && this.setOpenState(false)
+    //失焦
+    handleBlur = () => {
+        const _value = this.refs.input.props.value;
+
+
+        if(this.state.canMenuHide){
+            this.setOpenState(false);
+
+            // 失焦的时候如果input 中被back 清掉了值，则把select
+            !_value && this.setState({
+                selectKeys: ['-1'],
+            })
+        }
+
+        console.log(this)
     }
 
 
-    handleClick =(e)=>{
-        this.props.onChange &&　this.props.onChange(e.target.value);
+    handleClick = (e) => {
+        this.props.onChange && this.props.onChange(e.target.value);
         this.setOpenState(true)
     }
 
-    handleMenuMouseEnter=()=>{
+    handleMenuMouseEnter = () => {
         this.setState({
-            canMenuHide:false
+            canMenuHide: false
         })
     }
 
-    handleMenuMouseLeave=()=>{
+    handleMenuMouseLeave = () => {
         this.setState({
-            canMenuHide:true
+            canMenuHide: true
         })
         let input = ReactDOM.findDOMNode(this.refs.input).children[0]
         input.focus()
@@ -188,30 +202,33 @@ class MultiColSelect extends Component {
 
 
     render() {
-        const {dataHeader,dataBody,type,rows,autosize,...props} = this.props;
+        const {dataHeader, dataBody, type, rows, autosize, ...props} = this.props;
         const hasDataBody = dataBody && dataBody.length > 0;
         const dropdownHeadData = dataHeader,
             dropdownBodyData = hasDataBody ? dataBody : "not found";
 
         // 拼接下拉框header部分结构
-        const dropdownHeadElement = dropdownHeadData ? <MenuItem key="title" disabled>{dropdownHeadData.map(val => <p key={val.dataIndex}>{val.title}</p>)}</MenuItem> : '';
-        const dropdownBodyElement = !hasDataBody ? <MenuItem className="no-data" disabled><p colSpan="2">{dropdownBodyData}</p><p></p></MenuItem> : createBodyElement();
+        const dropdownHeadElement = dropdownHeadData ? <MenuItem key="title" disabled>{dropdownHeadData.map(val => <p
+            key={val.dataIndex}>{val.title}</p>)}</MenuItem> : '';
+        const dropdownBodyElement = !hasDataBody ?
+            <MenuItem className="no-data" disabled><p colSpan="2">{dropdownBodyData}</p><p></p>
+            </MenuItem> : createBodyElement();
         //下拉框body部分
         function createBodyElement() {
-            const isArray = dropdownBodyData instanceof  Array;
-            if(!isArray ){
+            const isArray = dropdownBodyData instanceof Array;
+            if (!isArray) {
                 throw  new TypeError('MultiColSelect 组件的 dataBody 只接受 "Array" 格式的数据');
             }
 
-            return dropdownBodyData.map(function (val,index) {
+            return dropdownBodyData.map(function (val, index) {
                 const parentVal = val;
-                const  _item = dropdownHeadData.map((val,i)=> <p key={i}>{parentVal[val.dataIndex]}</p>);
+                const _item = dropdownHeadData.map((val, i) => <p key={i}>{parentVal[val.dataIndex]}</p>);
                 return <MenuItem key={index}>{_item}</MenuItem>//这里的key可以当value使用，不用再定义value
             })
         }
 
-        const dropdwonMaxHeight = (props.dropdwonMaxRows +1) * this.props.scrollHeight;
-        const dropdwonStyle = props.dropdwonMaxRows && {maxHeight:dropdwonMaxHeight}
+        const dropdwonMaxHeight = (props.dropdwonMaxRows + 1) * this.props.scrollHeight;
+        const dropdwonStyle = props.dropdwonMaxRows && {maxHeight: dropdwonMaxHeight}
 
         return (
             <Dropdown overlay={
@@ -223,13 +240,13 @@ class MultiColSelect extends Component {
                     onMouseLeave={this.handleMenuMouseLeave}
                 >
                     <Menu
-                    onSelect={this.onMenuSelect}
-                    activeKey ={this.state.activeKey}
-                    selectedKeys={this.state.selectKeys}
-                >
-                    {dropdownHeadElement}
-                    {dropdownBodyElement}
-                </Menu>
+                        onSelect={this.onMenuSelect}
+                        activeKey={this.state.activeKey}
+                        selectedKeys={this.state.selectKeys}
+                    >
+                        {dropdownHeadElement}
+                        {dropdownBodyElement}
+                    </Menu>
 
                 </div>
             }
@@ -243,10 +260,10 @@ class MultiColSelect extends Component {
                     value={props.value}
                     rows={rows}
                     autosize={autosize}
-                    onClick ={this.handleClick}
+                    onClick={this.handleClick}
                     onChange={this.handleChange}
                     onKeyDown={this.onKeyDown}
-                    onBlur = {this.handleBlur}
+                    onBlur={this.handleBlur}
                 />
             </Dropdown>
         );

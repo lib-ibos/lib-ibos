36e9c76a5f83cbccb0fa69c6f71787b4bd117852
codering <zcodeworld@gmail.com>
2017-02-09 15:59:59 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
       root: [resolve("node_modules")],
       moduleTemplates: ['*-loader'],
     },
-    devtool: 'source-map', // 便于调试
+    devtool: 'cheap-source-map', // 便于调试
     module: {
         loaders: [
             {

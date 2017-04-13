3e30b36ed50908819e82909d3b3dea35faf38deb
fen <guangyaol@olymtech.com>
2017-04-13 17:19:03 +0800

    --version

--- samples/pages/Home.js
+++ samples/pages/Home.js
@@ -5,11 +5,16 @@ import 'ibos/styles/framework.less';
 import React, {Component} from 'react'
 
 import {Panel} from 'ibos'
+import {Select} from 'antd'
 // import router from './router'
+
+function test(a, b) {
+    console.log(a,b)
+}
 
 export default function Home() {
     return <Panel title="我是 Panel 标题">
-        speak async your
+        <Select onFirstFocus={test}/>
 
         <Panel noPadding title="我是 Panel 标题">
         123123

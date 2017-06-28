86e040dba6decb564a3d49cfb13a4447d94cd55c
fen <guangyaol@olymtech.com>
2017-06-28 14:04:11 +0800

    兼容 antd icon

--- src/components/ibos/icon/index.js
+++ src/components/ibos/icon/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import classnames from 'classnames'
 import './styles'
 export default props => {
-  let { type,color,size, className = '', ...other } = props;
+  let { lib,type,color,size, className = '', ...other } = props;
+  const preFix = lib === 'ibos' ? 'o-' : 'ant'
   const classNames = classnames(
     className,
-    `o-icon`,
-    `o-icon-${type}`,
+    `${preFix}icon`,
+    `${preFix}icon-${type}`,
     {
       [`color-${color}`]:color
     }
-  )
+  );
 
   const styles = size && {'fontSize':size};
 
